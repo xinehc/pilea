4ea98bd4291f1731a17e37cf8905958ec806f7a7
Xi Chen <xinehc@gmail.com>
2025-05-18 01:23:13 +0800

    reduce time/memory requirement for indexing

--- src/pilea/sketch.py
+++ src/pilea/sketch.py
@@ -2,40 +2,40 @@ import os
 import gzip
 
 from mmh3 import hash64
-from collections import Counter
+from collections import Counter, defaultdict
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 
-revcomp = bytes.maketrans(b"ACTG", b"TGAC")
+revcomp = bytes.maketrans(b'ACGT', b'TGCA')
+alphabet = set('ACGT')
 
 
-def canonicalize(seq, trans=revcomp):
+def canon(seq, trans=revcomp):
     return min(seq, seq[::-1].translate(trans))
+
+def count(seq, pos, flank=500):
+    seq = seq[max(0, pos - flank):pos + flank]
+    return int((seq.count('G') + seq.count('C')) / len(seq) * flank * 2)
 
 def sketch(file, folder, k, s, w):
     file, name, idx = file
-    maxhash = int((2 ** 64 - 1) / s)
+    maxhash = ((1 << 64) - 1) // s
 
-    ctgs = dict()
+    ctgs = defaultdict(list)
     kcnt = Counter()
     with gzip.open(file, 'rt') if '.gz' in file else open(file, 'r') as f:
         for n, (_, fseq) in enumerate(SimpleFastaParser(f)):
             fseq = fseq.upper()
-            kpos = [(canonicalize(fseq[i:i + k]), i + k // 2) for i in range(len(fseq) - k + 1)]
-            kcnt.update([kmer for kmer, _ in kpos])
-            ctgs[n] = (fseq, kpos)
+            kpos = [(i // w, (kmer, count(fseq, i + k // 2))) for i in range(len(fseq) - k + 1) if hash64((kmer := canon(fseq[i:i + k])), signed=False)[0] < maxhash and set(kmer) <= alphabet]
+            kcnt.update(kmer for _, (kmer, _) in kpos)
+            for i, j in kpos:
+                ctgs[f'{n}|{i}'].append(j)
 
     wins = 0
-    skes = set()
     kdup = {key for key, val in kcnt.items() if val > 1}
     with open(f'{folder}/{idx}.fa', 'w') as f:
-        for _, (fseq, kpos) in ctgs.items():
-            for i in range(0, len(kpos), w):
-                if ksub := [(kmer, pos, kmer in kdup) for kmer, pos in kpos[i:i + w] if hash64(kmer, signed=False)[0] < maxhash]:
-                    sins = sum(not dup for _, _, dup in ksub)
-                    for kmer, pos, dup in ksub:
-                        skes.add(kmer)
-                        gseq = fseq[max(0, pos - 500):pos + 500]
-                        gcnt = int((gseq.count('G') + gseq.count('C')) / len(gseq) * 1000)
-                        f.write(f'>{idx}|{wins}:{sins}|{gcnt}|{"-" if dup else "+"}\n{kmer}\n')
-                    wins += 1
-    return idx, (name, len(ctgs), wins, len(skes))
+        for widx, (_, kpos) in enumerate(ctgs.items()):
+            if (sins := sum(not kmer in kdup for kmer, _ in kpos)) > 1:
+                wins += 1
+            for kmer, gc in kpos:
+                f.write(f'>{idx}|{widx}:{sins}|{gc}|{"-" if kmer in kdup else "+"}\n{kmer}\n')
+    return idx, (name, len(set(i.split('|')[0] for i in ctgs.keys())), wins, len(kcnt) - len(kdup))
